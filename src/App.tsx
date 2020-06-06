ec3a57632b3d8bef1fa2fa006c41671258b840fa
Marc Schwering <schweringmarc01@gmail.com>
2020-06-06 18:06:59 +0200

    added maps embed

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import Iframe from 'react-iframe'
 
-function App() {
+
+function Header() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <header className={styles.AppHeader}>
+        <img src={logo} className={styles.AppLogo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={styles.AppLink}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
@@ -19,6 +20,30 @@ function App() {
           Learn React
         </a>
       </header>
+  );
+}
+
+
+function HomePage() {
+
+}
+
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <Header />
+      <div className="mapouter">
+        <div className="gmap_canvas">
+        <Iframe url="https://maps.google.com/maps?q=stranero%20berlin&t=&z=13&ie=UTF8&iwloc=&output=embed"
+          width="450px"
+          height="450px"
+          id="myId"
+          className="myClassname"
+          display="initial"
+          position="relative"/>
+          </div>
+        </div>
     </div>
   );
 }
