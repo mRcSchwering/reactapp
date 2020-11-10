1128a2b492b2dd8bbb42aef00dd08fbfae6c478d
marc <schweringmarc01@gmail.com>
2020-11-10 23:51:07 +0100

    got search that can do almost everything, need to refactor

--- src/components/SearchPanel.tsx
+++ src/components/SearchPanel.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./SearchPanel.module.css";
-import { useHistory } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 import { Nav } from "react-bootstrap";
 import { useQuery } from "@apollo/react-hooks";
 import { gql } from "apollo-boost";
@@ -16,52 +16,10 @@ const CHARACTER_NAMES_QUERY = gql`
   }
 `;
 
-function useSuggestedNames(
-  search: string | null
-): { name: string; id: number }[] {
-  const res = useQuery(CHARACTER_NAMES_QUERY);
-
-  if (res.data === null || search === null || search.length === 0) {
-    return [];
-  }
-
-  return res.data.characters.results
-    .map((d) => ({ name: d.name, id: d.id }))
-    .filter((d) => d.name.toLowerCase().includes(search));
-}
-
 type SuggestionsProps = {
   input: string;
   focus: boolean;
 };
-
-function Suggestions(props: SuggestionsProps): JSX.Element {
-  const names = useSuggestedNames(props.input);
-
-  if (names.length < 1 || !props.focus) {
-    return <div></div>;
-  }
-
-  const suggestionDivs = names.map((d) => (
-    <div className={styles.SuggestionItem} key={d.id}>
-      <Nav className="ml-auto">
-        <Nav.Link
-          className={styles.NavLink}
-          key="d"
-          href={`/browse?search=${d.name}&id=${d.id}`}
-        >
-          {d.name}
-        </Nav.Link>
-      </Nav>
-    </div>
-  ));
-
-  return (
-    <div className={styles.SuggestionsContainer}>
-      <div className={styles.Suggestions}>{suggestionDivs}</div>
-    </div>
-  );
-}
 
 type SearchBarProps = {
   input: string;
@@ -72,37 +30,113 @@ type SearchBarProps = {
   onBlur: () => void;
 };
 
+function useQueryStringParams() {
+  return new URLSearchParams(useLocation().search);
+}
+
+function useSuggestedNames(
+  search: string | null,
+  focus: boolean
+): { name: string; id: number }[] {
+  const res = useQuery(CHARACTER_NAMES_QUERY);
+
+  if (!focus || res.data === null || search === null || search.length === 0) {
+    return [];
+  }
+
+  return res.data.characters.results
+    .map((d) => ({ name: d.name, id: d.id }))
+    .filter((d) => d.name.toLowerCase().includes(search.toLowerCase()));
+}
+
+function Suggestions(props: SuggestionsProps): JSX.Element | null {
+  const names = useSuggestedNames(props.input, props.focus);
+  const history = useHistory();
+
+  if (!props.focus) {
+    return null;
+  }
+
+  if (props.input.length < 1) {
+    return (
+      <div className={styles.SuggestionsContainer}>
+        <div className={styles.GenericSuggestions}>
+          Some Generic suggestions
+        </div>
+      </div>
+    );
+  }
+
+  if (names.length < 1) {
+    return null;
+  }
+
+  function handleSuggestionClick(e: React.MouseEvent<HTMLDivElement>) {
+    const selection: string = e.target["textContent"];
+    history.push(`/browse?search=${selection}`);
+  }
+
+  const suggestionDivs = names.map((d) => (
+    <div
+      className={styles.SuggestionItem}
+      key={d.id}
+      onMouseDown={handleSuggestionClick}
+    >
+      {d.name}
+    </div>
+  ));
+
+  return (
+    <div className={styles.SuggestionsContainer}>
+      <div className={styles.Suggestions}>{suggestionDivs}</div>
+    </div>
+  );
+}
+
 function SearchBar(props: SearchBarProps) {
+  const searchBarStyle = props.focus
+    ? { borderColor: "rgb(142, 158, 142)" }
+    : { borderColor: "transparent" };
+
   return (
     <div
-      className={props.focus ? styles.SearchBarFocused : styles.SearchBar}
-      onFocus={(e) => props.onFocus()}
-      onBlur={(e) => props.onBlur()}
+      style={searchBarStyle}
+      className={styles.SearchBar}
+      onFocus={props.onFocus}
+      onBlur={props.onBlur}
     >
-      <div className={styles.SearchInputContainer}>
-        <input
-          value={props.input}
-          onChange={props.onChange}
-          onKeyDown={props.onKeyDown}
-          className={styles.SearchInput}
-        />
-      </div>
+      <input
+        value={props.input}
+        onChange={props.onChange}
+        onKeyDown={props.onKeyDown}
+        className={styles.SearchInput}
+      />
     </div>
   );
 }
 
 export default function SearchPanel(): JSX.Element {
   const history = useHistory();
-  const [input, setInput] = React.useState("");
+  const search = useQueryStringParams().get("search");
+
+  const [input, setInput] = React.useState(search ? search : "");
   const [inFocus, setInFocus] = React.useState(false);
+
+  const curtain = inFocus ? <div className={styles.curtain}></div> : null;
+
+  React.useEffect(() => {
+    setInput(search ? search : "");
+  }, [search]);
 
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
     setInput(e.target.value);
   }
 
   function handleKeyDown(e: React.KeyboardEvent) {
+    setInFocus(true);
     if (e.key === "Enter") {
       history.push(`/browse?search=${input}`);
+      setInFocus(false);
     }
   }
 
@@ -116,6 +150,7 @@ export default function SearchPanel(): JSX.Element {
         onFocus={() => setInFocus(true)}
         onBlur={() => setInFocus(false)}
       />
+      {curtain}
       <Suggestions input={input} focus={inFocus} />
     </div>
   );
